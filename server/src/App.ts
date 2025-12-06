d92a442b6faeb503a75eb7ca9c5db0ded0e9ee7d
azkbbys <oyroyroyr@163.com>
2025-12-06 12:18:32 +0800

    feat: 实现标准箱生成
    - 红色标准箱在全图范围随机生成
    - 标准箱数量上限10
    - 标准箱可碰撞
    - 开启OBB碰撞
    
    关联任务：#W1-002

--- server/src/App.ts
+++ server/src/App.ts
@@ -4,6 +4,26 @@ var walkSpeed = 0.5;
 var runSpeed = 0.5;
 var acceleration = 0.5;
 
+// 函数
+/**
+ * 获取随机整数
+ * 
+ * @param min 
+ * @param max 
+ * @returns 
+ */
+function randint(min:number, max:number){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+/**
+ * 获取在场所有箱子数量
+ * 
+ * @returns 箱子数量
+ */
+function getboxnumber(){
+    return world.querySelectorAll('#标准箱').length
+}
+
 world.onPlayerJoin(({entity})=>{
     entity.player.enableJump = false;
     entity.player.walkSpeed = walkSpeed;
@@ -11,3 +31,46 @@ world.onPlayerJoin(({entity})=>{
     entity.player.walkAcceleration = acceleration;
     entity.player.runAcceleration = acceleration;
 })
+
+// 开启OBB碰撞
+world.useOBB = true;
+
+// 生成标准箱
+world.onTick(({tick})=>{
+    if(tick % (16*3)===0){
+        if(getboxnumber()<10){
+            // if(randint(1,2)===1){
+                world.createEntity({
+                    mesh:'mesh/红色标准箱.vb',
+                    position:new GameVector3(randint(1,63), 10, randint(1,63)),
+                    meshScale:new GameVector3(0.05, 0.05, 0.05),
+                    collides:true, // 是否可碰撞
+                    fixed:false, // 是否固定
+                    gravity:true, // 是否受重力
+                    id:'标准箱',
+                })
+            // }
+            // else{
+                // world.createEntity({
+                //     mesh:'mesh/绿色标准箱.vb',
+                //     position:new GameVector3(randint(1,63), 10, randint(1,63)),
+                //     meshScale:new GameVector3(0.05, 0.05, 0.05),
+                //     collides:true, // 是否可碰撞
+                //     fixed:false, // 是否固定
+                //     gravity:true, // 是否受重力
+                //     id:'box_green',
+                // })
+            // }
+        }
+    }
+})
+
+// 收集箱子
+// world.onEntityContact(({entity, other})=>{
+//     if(!entity.player)return
+//     let e = entity as GamePlayerEntity
+//     if(other.id==='标准箱'){
+//         e.shouji+=1
+//         other.destroy()
+//     }
+// })
